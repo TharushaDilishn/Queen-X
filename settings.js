f44d397983fb4168380b6849a899663f23153471
TharushaDilishn <124675795+TharushaDilishn@users.noreply.github.com>
2023-02-07 16:26:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
-global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
-global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
+global.owner = ['94701643281'] //ur owner number
+global.ownername = "poodi Tipara"//ur owner name
+global.ytname = "xThru BB"//ur yt chanel name
+global.socialm = "poodi Tipara" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
-global.websitex = "https:// no" //ur website to be displayed
+global.ownernomer = "94701643281" //ur number
+global.premium = ['94701643281'] //ur premium number
+global.botname = 'xpodi Tipara' //ur bot name
+global.linkz = "https://chat.whatsapp.com/C3zSQjDbnEjC2GTnN5ua4f" //your theme url which will be displayed on whatsapp
+global.websitex = "https:// https://chat.whatsapp.com/C3zSQjDbnEjC2GTnN5ua4f" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.packname = "poodi Tipara" //ur sticker watermark packname
+global.author = "poodi Tipara\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
+global.wm = " poodi Tipara" //ur watermark
 
 // Other
 global.sessionName = 'session'
